beef70620c9e159074fe4b445a409ad9a8d65943
DaltonH <dheising@umich.edu>
2021-12-09 14:56:09 -0500

    File committted

---
+++ testSelectionSort.java
@@ -0,0 +1,17 @@
+package basicSelectionSort;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import junit.framework.TestCase;
+public class testSelectionSort extends TestCase {
+@Test
+
+public void test() {
+	testPositive();
+	testNegative();
+	testMixed();
+	testDuplicates();
+}
+    public testSelectionSort() {
+    }
+    
