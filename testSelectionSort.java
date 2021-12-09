f640e921434bd371595b94f21963d6952ea82e7d
DaltonH <dheising@umich.edu>
2021-12-09 14:57:41 -0500

    filed committed

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -14,4 +14,124 @@ public void test() {
 }
     public testSelectionSort() {
     }
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void testMixed(){
+    	  int[] arr = new int[5];
+          arr[0] = 0;
+          arr[1] = -12;
+          arr[2] = -4;
+          arr[3] = 20;
+          arr[4] = 2;
+          SelectionSort mixed = new SelectionSort();
+          int[] arraySorted = mixed.basicSelectionSort(arr);
+          int[] Sortedarr = new int[5];
+          Sortedarr[0] = -12;
+          Sortedarr[1] = -4;
+          Sortedarr[2] = 0;
+          Sortedarr[3] = 2;
+          Sortedarr[4] = 20;
+          assertEquals("Array is sorted", arraySorted[0], Sortedarr[0]);
+          System.out.print("Expected mixed array sorted: ");
+          for (int element: Sortedarr) {
+              System.out.print(element + " ");
+          }
+          System.out.println(" ");
+          System.out.print("Actual mixed array sorted: ");
+          for (int element: arraySorted) {
+              System.out.print(element + " ");
+          }
+          System.out.println(" ");
+        /** Test data contains with both positive, negative and zeros **/
+    }
+
+    public void testDuplicates(){
+    	  int[] arr = new int[5];
+          arr[0] = 0;
+          arr[1] = 12;
+          arr[2] = 12;
+          arr[3] = -5;
+          arr[4] = -5;
+          SelectionSort duplicate = new SelectionSort();
+          int[] arraySorted = duplicate.basicSelectionSort(arr);
+          int[] Sortedarr = new int[5];
+          Sortedarr[0] = -5;
+          Sortedarr[1] = -5;
+          Sortedarr[2] = 0;
+          Sortedarr[3] = 12;
+          Sortedarr[4] = 12;
+          assertEquals("Array is sorted", arraySorted[0], Sortedarr[0]);
+          System.out.print("Expected duplicate array sorted: ");
+          for (int element: Sortedarr) {
+              System.out.print(element + " ");
+          }
+          System.out.println(" ");
+          System.out.print("Actual duplicate array sorted: ");
+          for (int element: arraySorted) {
+              System.out.print(element + " ");
+          }
+          System.out.println(" ");
+        /** Test data contains duplicates **/
+    }
+}
